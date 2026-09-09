192a7854dfad6071a78b5924d286db4760064cea
Utsmanseff <117575172+Utsmanseff@users.noreply.github.com>
2026-09-10 03:36:18 +0800

    FIX

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
 						<div class="container-xxl d-flex flex-column flex-md-row flex-stack">
 							<div class="text-dark order-2 order-md-1">
 								<span class="text-gray-400 fw-bold me-1">Created by</span>
-								<a href="#" target="_blank" class="text-muted text-hover-primary fw-bold me-2 fs-6">Adinda Elisa Cahaya</a>
+								<a href="#" target="_blank" class="text-muted text-hover-primary fw-bold me-2 fs-6">Kecamatan Basarang</a>
 							</div>
 						</div>
 					</div>
